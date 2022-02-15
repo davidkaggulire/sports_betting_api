02db87407f7ed7b5e5d2877348cb334b0bd8b47e
davidkaggulire <dkaggulire@gmail.com>
2022-02-15 12:53:03 +0300

    add API key for route authentication

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -3,6 +3,7 @@
 from flask import jsonify, request
 from marshmallow import ValidationError
 from functools import wraps
+from flask import jsonify, abort, make_response,request
 
 
 def required_params(schema):
@@ -22,3 +23,16 @@ def required_params(schema):
 
         return wrapper
     return decorator
+
+
+def api_key_required(function):
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+        key = "1234"
+        print("the key is",key)
+        print("from headers",request.headers.get('api-key'))
+        if  request.headers.get('api-key') == key:
+            return function(*args, **kwargs)
+        else:
+            abort(make_response(jsonify(message="API key required"), 403))
+    return decorated_function
