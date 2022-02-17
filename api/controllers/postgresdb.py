4a8c585d6855a6091433c745cda636b6c065470b
davidkaggulire <dkaggulire@gmail.com>
2022-02-17 12:43:11 +0300

    postgresfile

--- api/controllers/postgresdb.py
+++ api/controllers/postgresdb.py
@@ -43,14 +43,14 @@ class PostgreSQLDatabase(IDatabase):
             print("Connection to PostgreSQL failed")
         return True
 
-    def disconnect(self):
-        if self.conn:
-            query = "DROP TABLE IF EXISTS contact"
-            self.cur.execute(query)
-            self.cur.close()
-            self.conn.close()
-            print("PostgreSQL connection is closed")
-        return super().disconnect()
+    # def disconnect(self):
+    #     if self.conn:
+    #         query = "DROP TABLE IF EXISTS contact"
+    #         self.cur.execute(query)
+    #         self.cur.close()
+    #         self.conn.close()
+    #         print("PostgreSQL connection is closed")
+    #     return super().disconnect()
 
     def create_table(self):
         create_table = ("CREATE TABLE IF NOT EXISTS odds"
@@ -92,7 +92,7 @@ class PostgreSQLDatabase(IDatabase):
                     draw_odds,
                     game_date
                 )
-                VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+                VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id;"""
             )
             record_to_insert = (
                 league,
@@ -103,11 +103,25 @@ class PostgreSQLDatabase(IDatabase):
                 draw_odds,
                 game_date
             )
-            self.cur.execute(insert_stmt, record_to_insert)
+            record_id = self.cur.execute(insert_stmt, record_to_insert)
             count = self.cur.rowcount
-            print(count, "Record inserted successfully into Odds table")
-            reason = "Data inserted into database successfully"
-            return True, reason
+            # getting record id
+            record = self.cur.fetchone()
+            sql_select_query = """SELECT * from odds where id=%s"""
+            self.cur.execute(sql_select_query, (record,))
+            # getting full record_object
+            record_data = self.cur.fetchone()
+
+            recordObject = []
+            columnNames = [column[0] for column in self.cur.description]
+
+            if record is not None:
+                created_object = dict(zip(columnNames, record_data))
+                recordObject.append(dict(zip(columnNames, record_data)))
+                print(recordObject)
+                print(count, "Record inserted successfully into Odds table")
+                reason = "Data inserted into database successfully"
+                return True, created_object
 
         except (Exception, psycopg2.Error) as error:
             print("Failed to insert record into mobile table: ", error)
@@ -158,6 +172,7 @@ class PostgreSQLDatabase(IDatabase):
                                 draw_odds=%s, game_date=%s
                                 where id=%s
                             """
+        odd_id = int(odd_id)
         data = (
             league,
             home_team,
@@ -174,6 +189,7 @@ class PostgreSQLDatabase(IDatabase):
         print(count, "Record Updated successfully ")
 
         print("Table After updating record ")
+        print(f"this is the {odd_id}")
         sql_select_query = """select * from odds where id = %s"""
         self.cur.execute(sql_select_query, (odd_id,))
         record = self.cur.fetchone()
@@ -183,9 +199,11 @@ class PostgreSQLDatabase(IDatabase):
         columnNames = [column[0] for column in self.cur.description]
 
         if record is not None:
+            updated_object = dict(zip(columnNames, record))
+            print(f"{updated_object} xxxxxxxxxxxxxxxxxxxxxx")
             recordObject.append(dict(zip(columnNames, record)))
             reason = "Data updated into database successfully"
-            return True, recordObject
+            return True, updated_object
 
         else:
             print("Error in update operation")
