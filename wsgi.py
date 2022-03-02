1e1cc63c8f20d4d82d9c7cef6fae8f8e7e4364e8
davidkaggulire <dkaggulire@gmail.com>
2022-03-02 12:24:50 +0300

    assign port from cloudrun

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ from api import create_app
 import os
 
 port = int(os.environ.get('PORT', 8080))
-app = create_app()
 print(f" the port is {port}")
+
+app = create_app()
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
