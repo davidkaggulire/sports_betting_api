fbbf5949329ab305f5027ab5c577a7f0e0d06a3c
davidkaggulire1 <dkaggulire@gmail.com>
2022-06-11 15:12:26 +0300

    add flask cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from . import views
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(views.main_bp)
 
